d7cb7295ee9e2d77684d2a146cc1c780410c543a
Ertyker <karpovic.arseni@gmail.com>
2024-11-16 13:16:16 +0300

    exam commit

--- Exam.cs
+++ Exam.cs
@@ -6,8 +6,26 @@
 }
 class Exam
 {
-    Person person;
-    Education education;
-    int grup;
+    string LessonName { get; set; }
+    int Mark {  get; set; }
+    DateTime DateExam { get; set; }
 
+    public Exam()
+    {
+        this.LessonName = "TRPO";
+        this.DateExam = new DateTime(2024,12,23,12,00,00);
+        this.Mark = 10;
+    }
+
+    public Exam(string lesName, int mark, DateTime dayEx)
+    {
+        this.Mark= mark;
+        this.DateExam = dayEx;
+        this.LessonName = lesName;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("Предмет: " + this.LessonName + " Оценка: " + this.Mark + " Дата проведения : " + this.DateExam);
+    }
 }

64868b461e20576e468ff61379002d5591e6de10
Ertyker <karpovic.arseni@gmail.com>
2024-11-18 14:23:29 +0300

    main commit

--- Main.cs
+++ Main.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeamProject;
 
-namespace TeamProject
-{
-    class Main
-    {
-    }
-}
+Student student = new Student();
+Console.WriteLine(student);
+Console.WriteLine(Education.Specialist + " " +Education.Вachelor +" "+ Education.SecondEducation);
+
+

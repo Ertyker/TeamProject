0419544a095fd87eaae712b72c09d323898aa0a8
Stepafold <ststrizhak@gmail.com>
2024-11-19 08:41:47 +0300

    new student commit

--- obj/Debug/net8.0/TeamProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TeamProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeamProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34209f2789522a9783cb428844a152ee728b9049")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f41438ab9b7e085aa5b93cac31eb8bbd0527b842")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeamProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeamProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
